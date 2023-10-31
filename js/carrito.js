a16d766ab646c9e9a948d0f94c002d1d06c71b4a
juanchoda200 <juan.berrio915@gmail.com>
2023-10-31 14:06:27 -0500

    31/10/23

--- js/carrito.js
+++ js/carrito.js
@@ -1,96 +1,78 @@
-// Obtener elementos del DOM
-const listaProductos = document.getElementById('lista-productos');
-const formularioProducto = document.getElementById('formulario-producto');
-const nombreProducto = document.getElementById('nombre-producto');
-const precioProducto = document.getElementById('precio-producto');
-const botonCompra = document.getElementById('boton-compra');
-const total = document.getElementById('total');
-
-// Inicializar arreglo de productos
-let productos = [];
-
-// Obtener productos almacenados en el localStorage
-if (localStorage.getItem('productos')) {
-    productos = JSON.parse(localStorage.getItem('productos'));
-    actualizarListaProductos();
-}
-
-// Agregar evento al formulario de producto
-formularioProducto.addEventListener('submit', function(evento) {
-    evento.preventDefault();
-
-    // Crear objeto producto
-    const producto = {
-        nombre: nombreProducto.value,
-        precio: parseFloat(precioProducto.value)
-    };
-
-    // Agregar producto al arreglo
-    productos.push(producto);
-
-    // Guardar productos en el localStorage
-    localStorage.setItem('productos', JSON.stringify(productos));
-
-    // Actualizar lista de productos
-    actualizarListaProductos();
-
-    // Limpiar formulario
-    nombreProducto.value = '';
-    precioProducto.value = '';
-});
-
-// Agregar evento al botón de compra
-botonCompra.addEventListener('click', function() {
-    // Calcular total de la compra
-    const totalCompra = productos.reduce(function(acumulador, producto) {
-        return acumulador + producto.precio;
-    }, 0);
-
-    // Mostrar total en la sección correspondiente
-    total.textContent = `Total: $${totalCompra.toFixed(2)}`;
-
-    // Vaciar arreglo de productos
-    productos = [];
-
-    // Guardar productos vacíos en el localStorage
-    localStorage.setItem('productos', JSON.stringify(productos));
-
-    // Actualizar lista de productos
-    actualizarListaProductos();
-});
-
-// Función para actualizar la lista de productos en el DOM
-function actualizarListaProductos() {
-    // Vaciar lista de productos en el DOM
-    listaProductos.innerHTML = '';
-
-    // Agregar cada producto a la lista en el DOM
-    productos.forEach(function(producto) {
-        const itemProducto = document.createElement('li');
-        itemProducto.textContent = `${producto.nombre} - $${producto.precio}`;
-        listaProductos.appendChild(itemProducto);
+// Array de productos
+const products = [
+    { id: 1, name: "Cepillos", brand: "Marca 1", price: 3000, image: "imageness/6ef0073f-61cc-495d-959b-d661d47c4421.png" },
+    { id: 2, name: "Producto 2", brand: "Marca 2", price: 200, image: "imageness/endo motor radar marca woodpecker.webp" },
+    { id: 3, name: "Producto 2", brand: "Marca 2", price: 200, image: "product2.jpg" },
+    { id: 4, name: "Producto 2", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  ];
+  
+  // Carrito de compras
+  let cart = [];
+  
+  // Función para mostrar productos en la página
+  function displayProducts() {
+    const productContainer = document.querySelector(".product-list");
+  
+    products.forEach((product, index) => {
+      const productDiv = document.createElement("div");
+      productDiv.classList.add("product", "product-container-2");
+      productDiv.setAttribute("id", product.id);
+  
+      // Muestra la imagen según la propiedad 'image' de cada producto
+      productDiv.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <h2>${product.name}</h2>
+        <p>Marca: ${product.brand}</p>
+        <p>Precio: $${product.price}</p>
+        <button class="add-to-cart" data-index="${index}">Añadir al carrito</button>
+      `;
+  
+      productContainer.appendChild(productDiv);
     });
-}
-
-// Obtener elementos del DOM para agregar al carrito
-const botonesAgregar = document.querySelectorAll('.product button');
-
-// Agregar evento a cada botón de agregar
-botonesAgregar.forEach(function(boton) {
-    boton.addEventListener('click', function() {
-        // Obtener información del producto
-        const producto = {
-            nombre: boton.parentElement.querySelector('h3').textContent,
-            precio: parseFloat(boton.parentElement.querySelector('p').textContent.slice(1))
-        };
-
-        // Agregar producto al arreglo
-        productos.push(producto);
-
-        // Guardar productos en el localStorage
-        localStorage.setItem('productos', JSON.stringify(productos));
-
-        // Actualizar lista de productos
-        actualizarListaProductos();
+  }
+  
+  // Función para actualizar el carrito de compras
+  function updateCart() {
+    const cartItems = document.getElementById("cart-items");
+    const cartTotal = document.getElementById("cart-total");
+    cartItems.innerHTML = "";
+    let total = 0;
+  
+    cart.forEach((item, index) => {
+      const li = document.createElement("li");
+      li.textContent = `${item.name} - $${item.price}`;
+      cartItems.appendChild(li);
+      total += item.price;
     });
-});
+  
+    cartTotal.textContent = total;
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  
+  // Cargar carrito y productos al iniciar la página
+  document.addEventListener("DOMContentLoaded", function () {
+    cart = JSON.parse(localStorage.getItem("cart")) || [];
+    displayProducts();
+    updateCart();
+  
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+  
+    addToCartButtons.forEach(function(button, index) {
+      button.addEventListener('click', function() {
+        const productToAdd = products[index];
+        cart.push(productToAdd);
+        updateCart();
+        localStorage.setItem("cart", JSON.stringify(cart));
+      });
+    });
+  
+    document.querySelectorAll(".product-container-2").forEach(product => {
+      product.addEventListener('click', () => {
+        const id = product.getAttribute("id");
+        const selectedProduct = products.find(x => x.id == id);
+        localStorage.setItem("PRODUCTO_ACTUAL", JSON.stringify(selectedProduct));
+        window.location = "./prevencion-diagnostico.html";
+      });
+    });
+  });
+  
--- js/prevencion-diagnostico.js
+++ js/prevencion-diagnostico.js
@@ -27,7 +27,6 @@ const searchInput = document.getElementById("search");
 const searchButton = document.getElementById("search-button");
 const searchResults = document.getElementById("search-results");
 
-
 // Función para realizar la búsqueda
 function performSearch() {
     const searchTerm = searchInput.value.toLowerCase();
@@ -83,3 +82,53 @@ function showPrevImage() {
 
 nextButton.addEventListener('click', showNextImage);
 prevButton.addEventListener('click', showPrevImage);
+
+// Obtener todas las etiquetas 'stars'
+const stars = document.querySelectorAll('.stars input');
+
+// Recorrer cada conjunto de estrellas
+stars.forEach(starSet => {
+  const inputs = starSet.querySelectorAll('input');
+
+  inputs.forEach(input => {
+    input.addEventListener('click', event => {
+      const selectedRating = event.target.value;
+      console.log(`Valoración seleccionada: ${selectedRating}`);
+      localStorage.setItem('selectedRating', selectedRating);
+      // Realizar más acciones con la valoración seleccionada si es necesario
+    });
+  });
+});
+
+// Obtener todos los botones de "Publicar comentario"
+const commentButtons = document.querySelectorAll('.review button');
+
+// Asignar eventos a los botones de "Publicar comentario"
+commentButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    const commentSection = event.target.parentNode;
+    const userComment = commentSection.querySelector('textarea').value;
+    const selectedImage = commentSection.querySelector('input[type="file"]').value;
+
+    console.log(`Comentario del usuario: ${userComment}`);
+    console.log(`Imagen seleccionada: ${selectedImage}`);
+    // Guardar el comentario y la imagen en el localStorage si es necesario
+  });
+});
+
+// Función para agregar productos al carrito
+function addToCart(index) {
+    const product = products[index];
+    cart.push(product);
+    updateCart();
+}
+
+// Manejar clic en "Ver detalles" y agregar al carrito
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("view-details")) {
+        const index = e.target.getAttribute("data-index");
+        showProductDetails(index);
+        addToCart(index); // Agregar el producto al carrito cuando se haga clic en "Ver detalles"
+    }
+});
+
