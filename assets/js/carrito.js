24ab294464bddc2792aa25f9f192d79df2366464
juanchoda200 <juan.berrio915@gmail.com>
2023-12-03 18:04:30 -0500

    3/12/23 2.5

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,251 +1,55 @@
-// Array de productos
-let products = JSON.parse(localStorage.getItem("listProductosCarrito")) || [];
+// Lista de productos
+const products = [
+  // ... (Tu lista de productos)
+];
 
-// Carrito de compras
+// Lista de productos en el carrito
 let cart = [];
-let productContainer = document.querySelector("#lista-productos");
 
-// Función para mostrar productos en la página
-function displayProducts() {
-  products.forEach((product, index) => {
-    let productDiv = document.createElement("div");
-
-    productDiv.classList.add("product", "product-container-2");
-    productDiv.setAttribute("id", product.id);
-
-    // Muestra la imagen según la propiedad 'image' de cada producto
-    productDiv.innerHTML = `
-      <img src="${product.image}" alt="${product.name}" width="100px">  
-      <h2>${product.name}</h2>
-      <p>Marca: ${product.brand}</p>
-      <p>Precio: $${product.price}</p>
-      <button class="add-to-cart" data-index="${index}">Eliminar del Carrito</button>`;
-    productContainer.appendChild(productDiv);
+// Función para procesar lista de productos y vista de estos
+function listarProductos() {
+  products.forEach(element => {
+    producto(element.id, element.name, element.brand, element.price, element.image);
   });
 }
 
-// Función para actualizar el carrito de compras
-function updateCart() {
-  const cartItems = document.getElementById("cart-items");
-  const cartTotal = document.getElementById("cart-total");
- 
-  let total = 0;
-
-  cart.forEach((item, index) => {
-    const li = document.createElement("li");
-    li.textContent = `${item.name} - $${item.price}`;
-    cartItems.appendChild(li);
-    total += item.price;
-  });
-
-  localStorage.setItem("cart", JSON.stringify(cart));
-}
-
-// Cargar carrito y productos al iniciar la página
-document.addEventListener("DOMContentLoaded", function () {
-  cart = JSON.parse(localStorage.getItem("cart")) || [];
-  displayProducts();
-  updateCart();
-
-  const addToCartButtons = document.querySelectorAll('.add-to-cart');
-
-  addToCartButtons.forEach(function (button, index) {
-    button.addEventListener('click', function () {
-      const productToAdd = products[index];
-      cart.push(productToAdd);
-      updateCart();
-      localStorage.setItem("cart", JSON.stringify(cart));
-    });
-  });
-
-  document.querySelectorAll(".product-container-2").forEach(product => {
-    product.addEventListener('click', () => {
-      const id = product.getAttribute("id");
-      const selectedProduct = products.find(x => x.id == id);
-      localStorage.setItem("PRODUCTO_ACTUAL", JSON.stringify(selectedProduct));
-      window.location = "./prevencion-diagnostico.html";
-    });
-  });
-});
-
-const menu = document.getElementById("menu")
-const toggleButton = document.getElementById("menu-toggle")
-const closeButton = document.getElementById('menu-cerrar')
-
-if (toggleButton) {
-  toggleButton.addEventListener('click', () => {
-    menu.classList.add('mostrar-menu')
-  })
-}
-
-if (closeButton) {
-  closeButton.addEventListener('click', () => {
-    menu.classList.remove('mostrar-menu')
-  })
-}
-
-const user = JSON.parse(localStorage.getItem("USURIO_ACTUAL"))
-
-if (user) {
-  document.getElementById("userName").innerText = user.nombre;
-}
-
-// En la pagina de inicio de incio (index.html). puedes mostrar el nombre de usuario
-function mostrarNombreUsuario() {
-  // Obtiene la información del usuario desde el localStorage
-  const user = JSON.parse(localStorage.getItem("USURIO_ACTUAL"));
-
-  // Obtiene referencias a elementos del DOM
-  const usernameDisplay = document.getElementById("username-display");
-  const infoUser = document.getElementById("user-info");
-  const loginitem = document.getElementById("login-item");
-
-  // Muestra la sección de información del usuario y oculta el botón de inicio de sesión
-  infoUser.style.display = "block";
-  loginitem.style.display = "none";
-
-  // Verifica si se obtuvo información válida del usuario y si existe un elemento para mostrar el nombre
-  if (user && usernameDisplay) {
-    // Actualiza el contenido del elemento con un mensaje de bienvenida
-    usernameDisplay.textContent = `Bienvenido, ${user.nombre}`;
-    // Puedes agregar aquí cualquier lógica adicional para mostrar la foto del usuario u otra información.
-  }
-
-  // Establece el estado de inicio de sesión en false (podría no ser necesario, dependiendo de tu lógica)
-  localStorage.setItem("login", false);
-}
-
-if (localStorage.getItem("login")) {
-  // Llama la función para mostrar el nombre del usuario en la página de inicio 
-  mostrarNombreUsuario();
-}
-
-function cerrarSesion() {
-  const infoUser = document.getElementById("user-info");
-  const loginItem = document.getElementById("login-item");
-
-  // Obtiene la información del usuario desde el localStorage
-  const user = JSON.parse(localStorage.getItem("USURIO_ACTUAL"));
-
-  // Muestra el nombre del usuario debajo de la sección de información del usuario
-  if (user && usernameDisplay) {
-    // Puedes ajustar aquí la presentación del nombre del usuario al cerrar sesión.
-    usernameDisplay.textContent = `Hasta luego, ${user.nombre}!`;
-  }
-
-  // Establece el estado de inicio de sesión en false
-  localStorage.setItem("login", false);
-
-  // Oculta la sección de información del usuario y muestra el botón de inicio de sesión
-  infoUser.style.display = "none";
-  loginItem.style.display = "block";
-
-  // Redirige a la página de inicio u otra página relevante después de cerrar sesión
-  window.location.href = "../../index.html";
-}
-
-function contarProductosEnCarrito() {
-  // Obtener el carrito de localStorage
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-  // Obtener la cantidad de productos en el carrito
-  let cantidadProductos = cart.length;
-
-  // Mostrar la cantidad (puedes ajustar esto según tus necesidades)
-  console.log(`Tienes ${cantidadProductos} productos en tu carrito.`);
-
-  // También podrías devolver la cantidad para usarla en otras partes de tu código
-  return cantidadProductos;
-}
-
-// Llama a la función para contar productos
-contarProductosEnCarrito();
-
-// Función para limpiar el carrito
-function limpiarCarrito() {
-  // Vacía el array cart
-  cart = [];
-
-  // Actualiza el carrito en el DOM
-  updateCart();
-
-  // Guarda el carrito vacío en el almacenamiento local
-  localStorage.setItem("cart", JSON.stringify(cart));
-}
-
-// Puedes llamar a esta función en respuesta a un evento, por ejemplo, al hacer clic en un botón:
-// Supongamos que tienes un botón
-
-// Función para resetear el almacenamiento local
-function resetearLocalStorage() {
-  localStorage.clear();
-  console.log("LocalStorage reseteado.");
-}
-
-// Llama a la función para resetear el almacenamiento local en respuesta a un evento, por ejemplo, al hacer clic en un botón:
-// Supongamos que tienes un botón con el id "resetear-localstorage-btn"
-const resetearLocalStorageBtn = document.getElementById("resetear-localstorage-btn");
-
-if (resetearLocalStorageBtn) {
-  resetearLocalStorageBtn.addEventListener("click", resetearLocalStorage);
-}
-
-// Agrega este bloque al final de tu archivo JavaScript
-document.getElementById('resetear-localstorage-btn').addEventListener('click', function () {
-  // Resetea el localStorage
-  localStorage.clear();
-
-  // Recarga la página para reflejar los cambios
-  location.reload();
-});
+let listaProductos = document.querySelector("#lista-productos");
 
 // Función para crear vista del producto
-function producto(name, brand, price, image) {
-  // Creamos un contenedor div para cada producto
-  const productContainer = document.createElement('div');
-  productContainer.className = 'product-container';
+function producto(id, name, brand, price, image) {
+  let productDiv = document.createElement("div");
 
-  // Creamos el elemento div con clase 'product'
-  const productDiv = document.createElement('div');
-  productDiv.className = 'product';
+  productDiv.classList.add("product", "product-container-2");
+  productDiv.setAttribute("id", id);
 
-  // Creamos elementos con características HTML
-  const imagen = document.createElement('img');
-  imagen.src = image;
-  imagen.alt = name;
-  imagen.className = 'imagen';
-
-  const nombre = document.createElement('h3');
-  nombre.innerText = name;
-
-  const precio = document.createElement('p');
-  precio.innerText = '$' + price.toFixed(2);
-
-  const boton = document.createElement('button');
-  boton.innerText = 'Agregar al Carrito';
-  boton.onclick = function () {
-    agregarAlCarrito(name, brand, price, image);
-  };
-
-  // Agregamos los elementos al contenedor
-  productDiv.appendChild(imagen);
-  productDiv.appendChild(nombre);
-  productDiv.appendChild(precio);
-  productDiv.appendChild(boton);
-
-  // Agregamos el elemento 'product' al contenedor principal
+  // Muestra la imagen según la propiedad 'image' de cada producto
+  productDiv.innerHTML = `
+    <img src="${image}" alt="${name}" width="100px">  
+    <h2>${name}</h2>
+    <p>Marca: ${brand}</p>
+    <p>Precio: $${price}</p>
+    <button class="add-to-cart" id="${id}" onclick="addCart(${id})">Agregar al carrito</button>`;
   productContainer.appendChild(productDiv);
-
-  // Pintamos el contenedor en el DOM
-  document.getElementById('lista-productos').appendChild(productContainer);
 }
 
 // Función para agregar al carrito
-function agregarAlCarrito(name, brand, price, image) {
-  const productToAdd = { name, brand, price, image };
-  cart.push(productToAdd);
-  updateCart();
-  localStorage.setItem('cart', JSON.stringify(cart));
+function addCart(id) {
+  cart.push(products[id]);
+  console.log(cart);
+  Cantidaddecarrito();
 }
 
-// ...
+// Función para mostrar la cantidad de productos en el carrito
+function Cantidaddecarrito() {
+  console.log(cart.length);
+}
+
+// Crear un contenedor para los productos
+const productContainer = document.createElement('div');
+productContainer.id = 'product-container';
+document.body.appendChild(productContainer);
+
+// Llamamos a la función para listar los productos al cargar la página
+document.addEventListener('DOMContentLoaded', function () {
+  listarProductos();
+});
--- assets/js/estética-pulido-y-rehabil.js
+++ assets/js/estética-pulido-y-rehabil.js
@@ -1,179 +1,160 @@
-// Productos
 const products = [
-    { id: 0, name: "TIRAS ABRASIVAS PLYESTER MICRODO", brand: "MICRODONT", price: 9750, image: "imageness/6ef0073f-61cc-495d-959b-d661d47c4421.png" },
-    { id: 1, name: "SR NEXCO OPACADOR A1", brand: "IVOCLAR VIVADENT", price: 11000, image: "imageness/endo motor radar marca woodpecker.webp" },
-    { id: 2, name: "SONDA CAROLINA DEL NORTE DELTA", brand: "DELTA", price: 70000, image: "product2.jpg" },
-    {id: 3, name: "RESINA TETRIC N-FLOW BULK FILL IVB", brand: "Ivoclar", price: 78400, image: "product2.jpg" },
-    {id: 4, name: "RESINA TETRIC N-FLOW BLEACH", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 5, name: "RESINA TETRIC N-FLOW A2", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 6, name: "RESINA TETRIC N-CERAM FLOW IVW", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 7, name: "RESINA SPECTRA BASIC A1", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 8, name: "RESINA HARMONIZE A1 ESMALTE JERINGA X", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 9, name: "RESINA EMPRESS BL-XL ENAMEL", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 10, name: "RESINA EMPRESS BL-XL DENTI", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 11, name: "RESINA EMPRESS B1 ENAMEL", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 12, name: "RESINA EMPRESS B1 DENTINA", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 13, name: "RESINA EMPRESS A2 DENTINA", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 14, name: "RATON * UND°", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "PUNTAS PARA RESINA X PAQUETE", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "PUNTAS MEZCLADORAS SILICONA", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "PUNTAS INTRAORALES SILICONA LIVIAN", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "PROTEMP 4 CARTUCHO COLOR A2", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "OPTRASCULPT PAD REFILL/6MM/60", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "MULTILINK N REFILL TRANSPARENTE", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "LIMAS 2DA SERIE 25MM K-FLEXO M-ACCES", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "LIMAS #35 25MM K-FLEXO M-ACCES", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "KIT REHABILITACION X 8", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "KIT PULIDO JOTA X 8", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "KIT PULIDO DISILICATO X5", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "KETAC MOLAR", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "HOJAS DE BISTURI #15C CAJA X 10", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "HOJAS DE BISTURI #12 CAJA X 100", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "HOJAS DE BISTURI #11 CAJA X 100", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "GUIA COLORES DURATON", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "GRAPA #0 DENT OPTIMA", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "ESPIGOS X UNIDAD", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "DISCOS SOFLEX ESSTENTIAL KIT", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "DISCO GRUESO DE PULIDO JIFFY", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "COPAS DE JIFFRY", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "CEMENTO VARIOLINK N CLEAR N LC REFILL", brand: "Marca 2", price: 200, image: "product2.jpg" },
-    {id: 3, name: "BRACKETS CERAMICOS 3M CLARITY", brand: "Marca 2", price: 200, image: "product2.jpg" },
-  ];
-    
-  // Carrito de compras
-  let cart = [];
-  
-  localStorage.setItem("listProductos", JSON.stringify(products));
-  
-  // Función para mostrar productos en la página
-  function displayProducts() {
-    const productContainer = document.querySelector(".product-list");
-  
-    products.forEach((product, index) => {
-      const productDiv = document.createElement("div");
-      productDiv.classList.add("product", "product-container-2");
-      productDiv.setAttribute("id", product.id);
-  
-      // Muestra la imagen según la propiedad 'image' de cada producto
-      productDiv.innerHTML = `
-        <img src="${product.image}" alt="${product.name}">
-        <h2>${product.name}</h2>
-        <p>Marca: ${product.brand}</p>
-        <p>Precio: $${product.price}</p>
-        <button class="view-details" data-index="${index}">Ver detalles</button>
-      `;
-  
-      productContainer.appendChild(productDiv);
-    });
-  }
-  
-  // Función para actualizar el carrito de compras
-  function updateCart() {
-    const cartItems = document.getElementById("cart-items");
-    const cartTotal = document.getElementById("cart-total");
-    /*cartItems.innerHTML = "";
-    let total = 0;
-  
-    cart.forEach((item, index) => {
-      const li = document.createElement("li");
-      li.textContent = `${item.name} - $${item.price}`;
-      cartItems.appendChild(li);
-      total += item.price;
-    });
-  
-    cartTotal.textContent = total;
-    localStorage.setItem("cart", JSON.stringify(cart));*/
-  }
-  
-  // Manejar clic en "Ver detalles"
-  document.addEventListener("click", function (e) {
-    if (e.target.classList.contains("view-details")) {
-      const index = e.target.getAttribute("data-index");
-      showProductDetails(index);
-    }
+  { id: 0, name: "TIRAS ABRASIVAS PLYESTER MICRODO", brand: "MICRODONT", price: 9750, image: "imageness/6ef0073f-61cc-495d-959b-d661d47c4421.png" },
+  { id: 1, name: "SR NEXCO OPACADOR A1", brand: "IVOCLAR VIVADENT", price: 11000, image: "imageness/endo motor radar marca woodpecker.webp" },
+  { id: 2, name: "SONDA CAROLINA DEL NORTE DELTA", brand: "DELTA", price: 70000, image: "product2.jpg" },
+  {id: 3, name: "RESINA TETRIC N-FLOW BULK FILL IVB", brand: "Ivoclar", price: 78400, image: "product2.jpg" },
+  {id: 4, name: "RESINA TETRIC N-FLOW BLEACH", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 5, name: "RESINA TETRIC N-FLOW A2", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 6, name: "RESINA TETRIC N-CERAM FLOW IVW", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 7, name: "RESINA SPECTRA BASIC A1", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 8, name: "RESINA HARMONIZE A1 ESMALTE JERINGA X", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 9, name: "RESINA EMPRESS BL-XL ENAMEL", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 10, name: "RESINA EMPRESS BL-XL DENTI", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 11, name: "RESINA EMPRESS B1 ENAMEL", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 12, name: "RESINA EMPRESS B1 DENTINA", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 13, name: "RESINA EMPRESS A2 DENTINA", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 14, name: "RATON * UND°", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "PUNTAS PARA RESINA X PAQUETE", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "PUNTAS MEZCLADORAS SILICONA", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "PUNTAS INTRAORALES SILICONA LIVIAN", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "PROTEMP 4 CARTUCHO COLOR A2", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "OPTRASCULPT PAD REFILL/6MM/60", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "MULTILINK N REFILL TRANSPARENTE", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "LIMAS 2DA SERIE 25MM K-FLEXO M-ACCES", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "LIMAS #35 25MM K-FLEXO M-ACCES", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "KIT REHABILITACION X 8", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "KIT PULIDO JOTA X 8", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "KIT PULIDO DISILICATO X5", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "KETAC MOLAR", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "HOJAS DE BISTURI #15C CAJA X 10", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "HOJAS DE BISTURI #12 CAJA X 100", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "HOJAS DE BISTURI #11 CAJA X 100", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "GUIA COLORES DURATON", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "GRAPA #0 DENT OPTIMA", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "ESPIGOS X UNIDAD", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "DISCOS SOFLEX ESSTENTIAL KIT", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "DISCO GRUESO DE PULIDO JIFFY", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "COPAS DE JIFFRY", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "CEMENTO VARIOLINK N CLEAR N LC REFILL", brand: "Marca 2", price: 200, image: "product2.jpg" },
+  {id: 3, name: "BRACKETS CERAMICOS 3M CLARITY", brand: "Marca 2", price: 200, image: "product2.jpg" },
+];
+
+//Funcion para procesar lista de productor y vista d estos
+function listarProductos() {
+  products.forEach(element => {
+    producto(element.id, element.name, element.brand, element.price, element.image);
   });
-  
-  // Función para mostrar detalles de un producto
-  function showProductDetails(index) {
-    const product = products[index];
-  
-    // Aquí puedes abrir una nueva página o un modal con los detalles del producto
-    // y permitir a los usuarios agregar el producto al carrito desde esa página.
-  }
-  
-  // Manejar comentarios
-  
-  
-  // Función para agregar comentarios
-  function addComment(commentText) {
-    const commentsList = document.querySelector("#comments ul");
-    const newComment = document.createElement("li");
-    newComment.innerHTML = `<p>Usuario:</p><p>${commentText}</p>`;
-    commentsList.appendChild(newComment);
-  }
-  
-  // Inicialización
-  document.addEventListener("DOMContentLoaded", function () {
-    // Cargar carrito desde localStorage, si existe
-    cart = JSON.parse(localStorage.getItem("cart")) || [];
-  
-    // Mostrar productos en la página
-    displayProducts();
-  
-    // Actualizar el carrito
-    updateCart();
+}
+
+let listaProductos = document.querySelector("#lista-productos");
+
+
+//Funcion para crear vista del producto
+function producto(id, name, brand, price, image) {
+
+  let productDiv = document.createElement("div");
+
+    productDiv.classList.add("product", "product-container-2");
+    productDiv.setAttribute("id", id);
+
+    // Muestra la imagen según la propiedad 'image' de cada producto
+    productDiv.innerHTML = `
+      <img src="${image}" alt="${name}" width="100px">  
+      <h2>${name}</h2>
+      <p>Marca: ${brand}</p>
+      <p>Precio: $${price}</p>
+      <button class="add-to-cart" id="${id}" onclick="addCart(${id})">Agregar al carrito</button>`;
+    productContainer.appendChild(productDiv); 
+}
+
+let cart = [];
+
+
+
+function addCart (id){
+  cart.push(products[id])
+  console.log(cart)
+  Cantidaddecarrito()
+}
+
+function Cantidaddecarrito (){
+  console.log (cart.length)
+}
+
+// Crear un contenedor para los productos
+const productContainer = document.createElement('div');
+productContainer.id = 'product-container';
+document.body.appendChild(productContainer);
+
+// Llamamos a la función para listar los productos al cargar la página
+document.addEventListener('DOMContentLoaded', function () {
+  listarProductos();
+});
+
+// Creamos un contenedor para cada producto
+const productDiv = document.createElement('div');
+productDiv.classList.add('product-container');
+
+const menu = document.getElementById("menu");
+const toggleButton = document.getElementById("menu-toggle");
+const closeButton = document.getElementById('menu-cerrar');
+const infoUser = document.getElementById("user-info");
+
+infoUser.style.display = "none";
+
+if (toggleButton) {
+  toggleButton.addEventListener('click', () => {
+    menu.classList.add('mostrar-menu');
   });
-  
-  // Elementos HTML
-  const searchInput = document.getElementById("search");
-  const searchButton = document.getElementById("search-button");
-  const searchResults = document.getElementById("search-results");
-  
-  
-  // Función para realizar la búsqueda
-  function performSearch() {
-    const searchTerm = searchInput.value.toLowerCase();
-    searchResults.innerHTML = "";
-  
-    if (searchTerm) {
-        const matchingProducts = products.filter(product => {
-            return product.name.toLowerCase().includes(searchTerm)
-        });
-  
-        if (matchingProducts.length > 0) {
-            matchingProducts.forEach(product => {
-                const resultItem = document.createElement("div");
-                resultItem.innerHTML = `<h3>${product.name}</h3><p>${product.description}</p>`;
-                searchResults.appendChild(resultItem);
-            });
-        } else {
-            searchResults.innerHTML = "<p>No se encontraron resultados.</p>";
-        }
-    } else {
-        searchResults.innerHTML = "<p>Ingrese un término de búsqueda.</p>";
-    }
-  }
-  
-  // Manejar clic en el botón de búsqueda
-  searchButton.addEventListener("click", performSearch);
-  
-  // Manejar la tecla Enter en el campo de búsqueda
-  searchInput.addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-        performSearch();
-    }
+}
+
+if (closeButton) {
+  closeButton.addEventListener('click', () => {
+    menu.classList.remove('mostrar-menu');
   });
-  
-  document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".product-container-2").forEach(product=>{
-    console.log(product);
-    product.addEventListener('click', () => {
-      const id = product.getAttribute("id")
-      
-      const selectedProduct = products.find(x=>x.id==id)
-      localStorage.setItem("PRODUCTO_ACTUAL", JSON.stringify(selectedProduct))
-      window.location = "./prevencion-diagnostico.html"
-    })
-  })
-  })
-  
+}
+
+const user = JSON.parse(localStorage.getItem("USURIO_ACTUAL"));
+
+if (user) {
+  document.getElementById("userName").innerText = user.nombre;
+}
+
+// En la pagina de inicio de incio (index.html). puedes mostrar el nombre de usuario
+function mostrarNombreUsuario() {
+  const userName = localStorage.getItem ("username");
+  const usernameDisplay = document.getElementById("username-display");
+  const infoUser = document.getElementById("user-info");
+  const loginitem = document.getElementById("login-item")
+ 
+  infoUser.style.display = "block";
+  loginitem.style.display = "none";
+
+  if (username && usernameDisplay) {
+
+    usernameDisplay.textContent = `Bienvenido, ${userName}!`;
+    // Agregar cualquier logica adicional para mostrar la foto del usuario u otra informacion.
+  }
+  localStorage.setItem("login", false);
+}
+
+if (localStorage.getItem("login") === "true") {
+  // Llama la funcion para mostrar el nombre del usuario en la pagina de inicio 
+  mostrarNombreUsuario()
+}
+
+function cerrarSesion() {
+  const infoUser = document.getElementById("user-info");
+  const loginItem = document.getElementById("login-item");
+
+
+  localStorage.setItem("login", false);
+  infoUser.style.display = "none";
+  loginItem.style.display = "block";
+}
+
+function agregarCarrito(){
+  const newProductCarrito = listProductos[currentIndex];
+  productosCarrito.push(newProductCarrito);
+  localStorage.setItem("listProductosCarrito", JSON.stringify(productosCarrito));
+}
